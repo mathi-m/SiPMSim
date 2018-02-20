9741e96a4b436cbc04e1033aedb6243090645929
mathi-m <36664203+mathi-m@users.noreply.github.com>
2018-02-20 14:44:58 +0100

    Init project

---
+++ include/Frame.h
@@ -0,0 +1,16 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+
+class Frame
+{
+    public:
+        Frame();
+        virtual ~Frame();
+
+    protected:
+
+    private:
+};
+
+#endif // FRAME_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Frame.h"
+
+//using namespace std;
+
+int main()
+{
+    int tester{100};
+    std::cout << "Hello world!" << std::endl;
+    std::cout << tester << std::endl;
+    return 0;
+}
